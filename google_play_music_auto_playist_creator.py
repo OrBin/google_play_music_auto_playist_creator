b62412f4277d819243387ae25e89d699c447c804
Or Bin <orbin50@gmail.com>
2016-10-02 01:18:08 +0300

    Initial commit

---
+++ google_play_music_auto_playist_creator.py
@@ -0,0 +1,50 @@
+from gmusicapi import Mobileclient
+import sys
+from datetime import datetime
+import time
+import logging
+
+# CONSTANTS
+HOURS_INTERVAL = 1 # Will run every hour
+CONFIG_FILE_NAME = ".google_play_music_auto_playlist_creator_config"
+PLAYLIST_NAME = "Non-played"
+
+
+logging.basicConfig(filename='log.txt',level=logging.INFO, format="%(asctime)s %(levelname)s %(module)s %(message)s", datefmt='%Y-%m-%d %H:%M:%S %Z')
+api = Mobileclient()
+
+with open(CONFIG_FILE_NAME) as conf_file:
+	username, password = conf_file.read().split("\n")[:2]
+	logged_in = api.login(username, password, Mobileclient.FROM_MAC_ADDRESS)
+
+	if not logged_in:
+		logging.error("Error: could not log in.")
+		sys.exit(1)
+
+all_songs = api.get_all_songs()
+
+non_played_songs = [song for song in all_songs if song["playCount"] == 0]
+
+all_playlists = api.get_all_playlists()
+
+playlist_id = None
+
+for playlist in all_playlists:
+	if playlist["name"] == PLAYLIST_NAME:
+		playlist_id = playlist["id"]
+		logging.info("Found playlist id: %s", playlist_id)
+		break
+
+if not playlist_id:
+	playlist_id = api.create_playlist(PLAYLIST_NAME)
+	logging.info("Created new playlist: id %s", playlist_id)
+
+all_playlists = api.get_all_user_playlist_contents()
+
+logging.info("Clearing the playlist")
+
+manipulated_playlist = [pl for pl in all_playlists if pl["name"] == PLAYLIST_NAME][0]
+api.remove_entries_from_playlist([track["id"] for track in manipulated_playlist["tracks"]])
+
+logging.info("Adding new tracks to the playlist")
+api.add_songs_to_playlist(playlist_id=playlist_id, song_ids=[song["id"] for song in non_played_songs])
